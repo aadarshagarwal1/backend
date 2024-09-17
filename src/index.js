6d0f2b2f6b631efcbeda11064fa963f1a3f46796
Aadarsh Agarwal <aadarshsaroon001@gmail.com>
2024-09-18 00:59:53 +0530

    utils added

--- src/index.js
+++ src/index.js
@@ -3,4 +3,16 @@ dotenv.config({
   path: "./env",
 });
 import connectDb from "../src/db/index.js";
-connectDb();
+import app from "./app.js";
+connectDb()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`listening at ${process.env.PORT || 3000}`);
+    });
+    app.get("/", (req, res) => {
+      res.send("apple");
+    });
+  })
+  .catch((err) => {
+    console.log("Database connection error", err);
+  });
