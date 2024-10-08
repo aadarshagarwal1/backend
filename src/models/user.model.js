24a6d65ba739311f8013aa77eefa4f80082336c4
Aadarsh Agarwal <aadarshsaroon001@gmail.com>
2024-10-08 23:42:29 +0530

    config

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,8 @@
 import mongoose, { Schema } from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+dotenv.config();
 const userSchema = new Schema(
   {
     username: {
