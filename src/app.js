44e3923e954e7ecfee0eb170d0e6cd381421fe12
Aadarsh Agarwal <aadarshsaroon001@gmail.com>
2024-11-06 18:32:43 +0530

    like controller and routes setup

--- src/app.js
+++ src/app.js
@@ -21,7 +21,9 @@ app.use(cookieParser());
 //routes import
 import { router as userRouter } from "./routes/user.routes.js";
 import { router as commentRouter } from "./routes/comment.routes.js";
+import { router as likeRouter } from "./routes/like.routes.js";
 //routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
 export default app;
