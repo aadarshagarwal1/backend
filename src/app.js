e913ecb62445b0ad65e75b2a490aa30ccd8f4590
Aadarsh Agarwal <aadarshsaroon001@gmail.com>
2024-11-08 21:28:59 +0530

    healthcheck controller and route setup

--- src/app.js
+++ src/app.js
@@ -26,6 +26,7 @@ import { router as tweetRouter } from "./routes/tweet.routes.js";
 import { router as playlistRouter } from "./routes/playlist.routes.js";
 import { router as subscriptionRouter } from "./routes/subscription.routes.js";
 import { router as videoRouter } from "./routes/video.routes.js";
+import { router as healthcheckRouter } from "./routes/healthcheck.routes.js";
 //routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/comments", commentRouter);
@@ -34,4 +35,5 @@ app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/playlists", playlistRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/healthcheck", healthcheckRouter);
 export default app;
